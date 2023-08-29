f55c3ccce1fd3dd5b916fef4b300490413cca2fa
Aditya Rajmane <anr331@nyu.edu>
2023-08-29 16:40:57 +0530

    uploaded from desk

---
+++ PrintNumber.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declaring an int type variable
+    int a;
+    // Assigning a value in a variable
+    a = 20;
+    // Printing the value of a variable
+    printf("Variable a is %d", a);
+    return 0;
+}
---
+++ UserInputBasic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    //make variables
+    int a = 5;
+    int new_a;
+    //print old value
+    printf("The current value of a is: %d\nChange the value of a to whatever you like - \n",a);
+    //ask for new value
+    scanf("%d",&new_a);
+    //print new value
+    printf("The New Value is: %d\n",new_a);
+}
